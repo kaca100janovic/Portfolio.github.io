47d56596e96e54ee201d7ae26874422829be41d4
kaca100janovic <kaca.katarina.stojanovic@gmail.com>
2023-10-25 23:43:20 +0200

    add js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Mendix Developer", "Test Engineer", "Frontend Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    startDelay: 1000,
+    loop: true
+});
